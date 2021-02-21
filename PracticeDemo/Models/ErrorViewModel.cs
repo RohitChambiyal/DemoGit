17341d5eef1d2a7028549d2d6a7201e832b4e853
Rohit Singh <17bcs1092@cuchd.in>
2021-02-21 21:09:33 +0530

    Done

---
+++ PracticeDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PracticeDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
